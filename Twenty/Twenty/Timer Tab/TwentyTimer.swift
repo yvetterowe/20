a02dce7030eb7ef72aa6d672e183c164c1f57265
Hao Luo <yvetterowe1116@gmail.com>
2020-05-29 20:30:38 -0400

    real timer w/ BackgroundTimer

--- Twenty/Twenty/Timer Tab/TwentyTimer.swift
+++ Twenty/Twenty/Timer Tab/TwentyTimer.swift
@@ -23,3 +23,51 @@ protocol TwentyTimer {
     var state: AnyPublisher<TimerState, Never> { get }
     func sendAction(_ timerAction: TimerAction)
 }
+
+final class RealTimer: TwentyTimer {
+    private let timeInterval: TimeInterval
+    @Published private var internalState: TimerState {
+        didSet {
+            print("\(internalState)")
+        }
+    }
+    
+    private lazy var backgroundTimer: BackgroundTimer = .init(timeInterval: timeInterval) {
+        guard case let .active(totalActiveTime) = self.internalState else {
+            return
+        }
+        self.internalState = .active(totalActiveTime + self.timeInterval)
+    }
+    
+    init(timeInterval: TimeInterval = 1, totalActiveTime: TimeInterval) {
+        self.timeInterval = timeInterval
+        self.internalState = .inactive(totalActiveTime)
+    }
+    
+    // MARK: - TwentyTimer
+    
+    var state: AnyPublisher<TimerState, Never> {
+        return $internalState
+            .receive(on: RunLoop.main)
+            .eraseToAnyPublisher()
+    }
+    
+    func sendAction(_ timerAction: TimerAction) {
+        switch timerAction {
+        case .start:
+            guard case let .inactive(timeInterval) = internalState else {
+                return
+            }
+            internalState = .active(timeInterval)
+            backgroundTimer.resume()
+            
+        case .pause:
+            guard case let .active(timeInterval) = internalState else {
+                return
+            }
+            internalState = .inactive(timeInterval)
+            backgroundTimer.suspend()
+        }
+    }
+    
+}
