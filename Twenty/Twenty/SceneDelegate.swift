c74f470e9d57d860b02f76509c21706539ab21d5
Hao Luo <yvetterowe1116@gmail.com>
2020-06-10 20:58:59 -0400

    Use real today as current date

--- Twenty/Twenty/SceneDelegate.swift
+++ Twenty/Twenty/SceneDelegate.swift
@@ -21,7 +21,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
 
         // Create the SwiftUI view that provides the window contents.
-        let currentDate: Date.Day = MockTimerFactory.currentDate.asDay(in: .current)
+        let currentDate: Date.Day = Date().asDay(in: .current)
         let (goalStoreReader, goalStoreWriter) = MockGoalFactory.makeGoalReaderAndWriter()
         let goalID = "goal-0"
         let goalPublisher: GoalPublisher = goalStoreReader.goalPublisher(for: goalID)
