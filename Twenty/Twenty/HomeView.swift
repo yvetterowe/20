cc7883a08fcece48df6f5ef98cfdc1a1fc6afaba
Effy Zhang <effyyzhang@gmail.com>
2020-06-15 21:28:45 -0600

    add utilities

---
+++ Twenty/Twenty/HomeView.swift
@@ -0,0 +1,45 @@
+//
+//  HomeView.swift
+//  Twenty
+//
+//  Created by Effy Zhang on 6/14/20.
+//  Copyright © 2020 Hao Luo. All rights reserved.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+        private let timerTabContext: StatefulTimerTabView.Context
+        @State private var selection = 0
+        init(timerTabContext: StatefulTimerTabView.Context) {
+            self.timerTabContext = timerTabContext
+        }
+    var body: some View {
+                TabView(selection: $selection){
+                    StatefulTimerTabView(context: timerTabContext)
+                        .font(.title)
+                        .tabItem {
+                            VStack {
+                                Image("first")
+                                Text("TwentyTimer")
+                            }
+                        }
+                        .tag(0)
+                    Text("Second View")
+                        .font(.title)
+                        .tabItem {
+                            VStack {
+                                Image("second")
+                                Text("Second")
+                            }
+                        }
+                        .tag(1)
+                }
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+       Text("Hello, World!")
+    }
+}
--- Twenty/Twenty/VIewController/SignupViewController.swift
+++ Twenty/Twenty/VIewController/SignupViewController.swift
@@ -102,9 +102,7 @@ class SignUpViewController: UIViewController {
     
     func transitionToHome(){
         let homeViewController = storyboard?.instantiateViewController(identifier: Constants.Storyboard.homeViewController) as? HomeViewController
-        
         view.window?.rootViewController = homeViewController
-
         view.window?.makeKeyAndVisible()
     }
 }
