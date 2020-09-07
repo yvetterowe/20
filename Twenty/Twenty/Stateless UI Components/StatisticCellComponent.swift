1485146c9ff5788caaadb2f0538762947a6a3684
Hao Luo <yvetterowe1116@gmail.com>
2020-09-07 11:41:44 -0400

    Extract StatisticCellComponent

---
+++ Twenty/Twenty/Stateless UI Components/StatisticCellComponent.swift
@@ -0,0 +1,39 @@
+//
+//  StatisticCellComponent.swift
+//  Twenty
+//
+//  Created by Hao Luo on 9/7/20.
+//  Copyright © 2020 Hao Luo. All rights reserved.
+//
+
+import SwiftUI
+
+struct StatisticCellComponent: View {
+    let icon: Image
+    let title: String
+    let subtitle: String
+    
+    var body: some View {
+        HStack {
+            icon.resizable()
+                .aspectRatio(contentMode: .fit)
+            VStack(alignment: .leading) {
+                Text(title)
+                Text(subtitle)
+            }
+        }
+    }
+}
+
+struct StatisticCellComponent_Previews: PreviewProvider {
+    static var previews: some View {
+        StatisticCellComponent(
+            icon: Image(systemName: "number.square"),
+            title: "1h 15m",
+            subtitle: "Avg everyday"
+        )
+        .frame(width: 180, height: 40, alignment: .leading)
+        .border(Color.red)
+        
+    }
+}
