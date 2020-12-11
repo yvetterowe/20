c559c6517c64525c622b686fb071b6275e36c12b
Hao Luo <yvetterowe1116@gmail.com>
2020-12-10 20:52:14 -1000

    Kill tab view

--- Twenty/Twenty/ContentView.swift
+++ Twenty/Twenty/ContentView.swift
@@ -12,56 +12,35 @@ import SwiftUI
 struct ContentView: View {
     private let timerTabContext: Context
 
-    @State private var selection = 0
-
     init(timerTabContext: Context) {
         self.timerTabContext = timerTabContext
     }
     
     var body: some View {
-        TabView(selection: $selection) {
-            StatefulDayView(
-                context: timerTabContext,
-                viewStateStore: ObservableWrapper(
-                    publisher: DayViewModelStore(
-                        selectedDayPublisher: timerTabContext.selectDayStore.selectDayPublisher,
-                        todayPublisher: timerTabContext.todayPublisher,
-                        goalPublisher: timerTabContext.goalPublisher
-                    ).publisher
-                )
-            ) { presentingTimer in
-                
-                let timerStateStore = TimerStateStore(initialState: .inactive(.init()))
-                let timerViewStateStore = TimerViewStateStore(
-                    timerStatePublisher: timerStateStore.timerStatePublisher,
-                    timerStateWriter: timerStateStore,
-                    goalStoreWriter: timerTabContext.goalStoreWriter,
-                    goalID: timerTabContext.goalID
-                )
-                
-                StatefulTimerView(
-                    viewStateReader: .init(publisher: timerViewStateStore.publisher),
-                    timerViewModelWriter: timerViewStateStore,
-                    presentingTimer: presentingTimer
-                )
-            }
-            .font(.title)
-            .tabItem {
-                VStack {
-                    Image("first")
-                    Text("TwentyTimer")
-                }
-            }
-            .tag(0)
-            Text("Second View")
-                .font(.title)
-                .tabItem {
-                    VStack {
-                        Image("second")
-                        Text("Second")
-                    }
-                }
-                .tag(1)
+        StatefulDayView(
+            context: timerTabContext,
+            viewStateStore: ObservableWrapper(
+                publisher: DayViewModelStore(
+                    selectedDayPublisher: timerTabContext.selectDayStore.selectDayPublisher,
+                    todayPublisher: timerTabContext.todayPublisher,
+                    goalPublisher: timerTabContext.goalPublisher
+                ).publisher
+            )
+        ) { presentingTimer in
+            
+            let timerStateStore = TimerStateStore(initialState: .inactive(.init()))
+            let timerViewStateStore = TimerViewStateStore(
+                timerStatePublisher: timerStateStore.timerStatePublisher,
+                timerStateWriter: timerStateStore,
+                goalStoreWriter: timerTabContext.goalStoreWriter,
+                goalID: timerTabContext.goalID
+            )
+            
+            StatefulTimerView(
+                viewStateReader: .init(publisher: timerViewStateStore.publisher),
+                timerViewModelWriter: timerViewStateStore,
+                presentingTimer: presentingTimer
+            )
         }
     }
 }
