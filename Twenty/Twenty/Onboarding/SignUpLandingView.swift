d5cb0118ca50c348db34a3be6ecb5cd3a462f09b
Effy Zhang <effyyzhang@gmail.com>
2021-02-16 23:40:48 -0800

    styled some texts

--- Twenty/Twenty/Onboarding/SignUpLandingView.swift
+++ Twenty/Twenty/Onboarding/SignUpLandingView.swift
@@ -16,36 +16,40 @@ struct SignUpLandingView: View {
     
     var body: some View {
         NavigationView {
-            VStack {
-                OnboardingTitleLabelComponent(title: "Learn Something new in 20hrs")
-                OnboardingSubtitleLabelComponent(subtitle: "Research shows...")
-                Spacer()
-                NavigationLink(
-                    destination: EmailSignUpView(store: .init(authService: authService)),
-                    isActive: $signUpButtonTapped
-                ){
-                    Button("Sign up") {
-                        signUpButtonTapped = true
+            ZStack(alignment: .topLeading){
+                ColorManager.Blue.edgesIgnoringSafeArea(.all)
+                VStack(alignment: .leading, spacing: 20){
+                    HStack{
+                        Text("TWENTY")
+                            .foregroundColor(ColorManager.White)
                     }
-                }
-                Spacer()
-                HStack {
-                    OnboardingSubtitleLabelComponent(subtitle: "Already have an account?")
+                    OnboardingTitleLabelComponent(title: "Learn something new in 20hrs")
+                    OnboardingSubtitleLabelComponent(subtitle: "That learning curve differs immensely between various skills but Kauffman found that most skills can be acquired, at least at a basic level of proficiency, within just 20 hours. Just 20 hours of deliberate, focused practise is all you really need to build basic proficiency in any new skill.")
+                    
                     NavigationLink(
-                        destination: EmailSignInView(store: .init(authService: authService)),
-                        isActive: $signInButtonTapped
+                        destination: EmailSignUpView(store: .init(authService: authService)),
+                        isActive: $signUpButtonTapped
                     ){
-                        Button("Sign in") {
-                            signInButtonTapped = true
+                        Button("Sign up") {
+                            signUpButtonTapped = true
+                        }
+                    }
+                    HStack {
+                        OnboardingSubtitleLabelComponent(subtitle: "Already have an account?")
+                        NavigationLink(
+                            destination: EmailSignInView(store: .init(authService: authService)),
+                            isActive: $signInButtonTapped
+                        ){
+                            Button("Sign in") {
+                                signInButtonTapped = true
+                            }
+                            .foregroundColor(ColorManager.White)
+                            
                         }
                     }
                 }
+                .padding(20)
             }
-            .padding(20)
-            .background(ColorManager.Blue)
-            .ignoresSafeArea(.all)
-            
-
         }
     }
 }
